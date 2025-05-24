ef495c5ed907687c5aea07fc3359baf23acf2e9f
tutunak <dk@tutunak.com>
2025-05-24 16:56:20 +0200

    add initial implementation of sigrab CLI tool with main and root command

---
+++ cmd/sigrab/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// This is a placeholder for the main function.
+	// The actual implementation will be added later.
+	// For now, we can just print a message or perform a simple operation.
+	println("sigrab command-line tool is running...")
+	// Additional functionality will be implemented in the future.
+}
